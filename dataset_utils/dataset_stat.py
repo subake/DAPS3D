9d9e072757db97a27b590b067d5b9efd27c59c70
Alex Khorin <alexkhorin199932@gmail.com>
2023-05-07 12:23:52 +0300

    Datastats Script Update

--- dataset_utils/dataset_stat.py
+++ dataset_utils/dataset_stat.py
@@ -27,6 +27,28 @@ def load_clouds(dataset_path, cfg):
                         print('Error extension: %s' % cloud_file_path)
                         continue
 
+def load_labels(dataset_path, cfg):
+    NAV = yaml.safe_load(open(path.join(cfg, 'navigation.yml'), 'r'))
+
+    for dataset_name in NAV:
+        for split in NAV[dataset_name]:
+            if not NAV[dataset_name][split]:
+                continue
+            for seq in NAV[dataset_name][split]:
+                dir_path = path.join(dataset_path, dataset_name, 'data', str(seq), 'labels')
+                print('Reading: %s' % dir_path)
+                label_file_names = sorted(listdir(dir_path))
+                for label_file_name in tqdm(label_file_names):
+                    label_file_path = path.join(dir_path, label_file_name)
+                    # load label
+                    if label_file_path.endswith('.label'):
+                        yield np.fromfile(label_file_path, dtype=np.int32).reshape((-1))
+                    elif label_file_path.endswith('.npy'):
+                        yield np.load(label_file_path, allow_pickle=True)
+                    else:
+                        print('Error extension: %s' % label_file_path)
+                        continue
+
 def calculate_mean_stats(cloud):
     range_sum = np.sum(np.linalg.norm(cloud[:, :3], axis=1))
     coord_sums = np.sum(cloud, axis=0)
@@ -61,15 +83,33 @@ def calculate_stats(dataset_path, cfg):
     ddof = 1
     stds[:5] = np.sqrt(stds[:5] / (stds[5] - ddof))
 
+    # calc content
+    labl_count = {}
+    print('Calculating content...')
+    for label in load_labels(dataset_path, cfg):
+        lbls, cnt = np.unique(label, return_counts=True)
+        for i, lbl in enumerate(lbls):
+            if lbl not in labl_count:
+                labl_count[lbl] = cnt[i]
+            else:
+                labl_count[lbl] += cnt[i]
+
+    labl_count = {k: labl_count[k] for k in sorted(labl_count.keys())}
+    print(labl_count)
+    total_count = sum(labl_count.values())
+
     # showing results
     print('PC count: %s' % num_clouds)
     keys = ['r', 'x', 'y', 'z', 'i']
-    print('Means:')
+    print('img_means:')
     for key, mean in zip(keys, means[:5]):
-        print('\t%s: %.2f' % (key, mean))
-    print('STDs:')
+        print('\t- %.2f' % (mean))
+    print('img_stds:')
     for key, std in zip(keys, stds[:5]):
-        print('\t%s: %.2f' % (key, std))
+        print('\t- %.2f' % (std))
+    print('content:')
+    for key in labl_count.keys():
+        print('\t%s: %.16f' % (key, labl_count[key]/total_count))
 
 # FIXME: need IMAGE stats, not clouds!
     
